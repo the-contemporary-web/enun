70b6800e7adf075352fb42689198d58ec641b4bc
designDefined <designdefined.by@gmail.com>
2025-05-19 03:35:07 +0900

    feat: Allow composed stores to provider

--- package/react/src/context/StoreProvider.tsx
+++ package/react/src/context/StoreProvider.tsx
@@ -1,22 +1,28 @@
-import { StoreApi } from "@enun/store";
+import { Composed, Storable, StoreApi } from "@enun/store";
 import { PropsWithChildren, useContext, useMemo } from "react";
 
 import { StoreContext, StoreMap } from "./StoreContext";
 
 interface StoreProviderProps extends PropsWithChildren {
-  store: StoreApi<object>[];
+  store?: StoreApi<Storable>[];
+  composed?: Composed<Storable>[];
 }
 
-const StoreProvider = ({ children, store }: StoreProviderProps) => {
+const StoreProvider = ({ children, store = [], composed = [] }: StoreProviderProps) => {
   const { storeMap } = useContext(StoreContext);
-  const newStoreMap = useMemo(
-    () => toStoreMap(store),
-    store.map(s => s.key),
-  );
+  const newStoreMap = useMemo(() => {
+    return {
+      ...storeToStoreMap(store),
+      ...composedToStoreMap(composed),
+    };
+  }, [...store.map(s => s.key), ...composed.map(([s]) => s.key)]);
   return <StoreContext value={{ storeMap: { ...storeMap, ...newStoreMap } }}>{children}</StoreContext>;
 };
 
-const toStoreMap = (stores: StoreApi<object>[]): StoreMap =>
+const storeToStoreMap = (stores: StoreApi<object>[]): StoreMap =>
   Object.fromEntries(stores.map(store => [store.fingerPrint, store]));
+
+const composedToStoreMap = (composed: Composed<Storable>[]): StoreMap =>
+  Object.fromEntries(composed.map(([api]) => [api.fingerPrint, api]));
 
 export { StoreProvider };
